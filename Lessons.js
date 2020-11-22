cd91b8b281d4ddfd0ec32fef4e35866b0d434f0b
ArslanWork1 <73851312+ArslanWork1@users.noreply.github.com>
2020-11-22 12:21:50 +0000

    Add files via upload

---
+++ Lessons.js
@@ -0,0 +1,111 @@
+{
+    "lessons";[
+        {
+            id: 101, 
+            title:"Maths",
+            location:"Location: London",
+            price: "Price: £" + 100,
+            spaces: "Number of spaces: ", 
+            spaceNumber: 5,
+            image:"./Images/Maths1.png", 
+        },
+
+        {
+            id: 102, 
+            title:"English",
+            location:"Location: South London",
+            price: "Price: £" + 90,
+            spaces: "Number of spaces: ", 
+            spaceNumber: 5,
+            image:"./Images/English.png", 
+        },
+        {
+            id: 103, 
+            title:"Science",
+            location:"Location: East London",
+            price: "Price: £" + 105,
+            spaces: "Number of spaces: ", 
+            spaceNumber: 5,
+            image:"./Images/Science.png", 
+
+        },
+        {
+            id: 104, 
+            title:"Geography",
+            location:"Location: North west London",
+            price: "Price: £" + 85,
+            spaces: "Number of spaces: ", 
+            spaceNumber: 5,
+            image:"./Images/Geography.png", 
+
+
+
+        },
+        {
+            id: 105, 
+            title:"ICT",
+            location:"Location: Reading",
+            price: "Price: £" + 95,
+            spaces: "Number of spaces: ", 
+            spaceNumber: 5,
+            image:"./Images/ict.png", 
+
+        },
+        {
+            id: 106, 
+            title:"Robotics",
+            location:"Location: Essex",
+            price: "Price: £" + 105,
+            spaces: "Number of spaces: ", 
+            spaceNumber: 5,
+            image:"./Images/Robot.png", 
+
+        },
+        {
+            id: 107, 
+            title:"Photogrpahy",
+            location:"Location: Luton",
+            price: "Price: £" + 105,
+            spaces: "Number of spaces: ", 
+            spaceNumber: 5,
+            image:"./Images/Photogrpahy.png", 
+
+
+
+        },
+        {
+            id: 108, 
+            title:"Dancing",
+            location:"Location: Central London",
+            price: "Price: £" + 110,
+            spaces: "Number of spaces: ", 
+            spaceNumber: 5,
+            image:"./Images/Dancing.png", 
+
+
+            
+        },
+        {
+            id: 109, 
+            title:"French",
+            location:"Location: Manchester",
+            price: "Price: £" + 120,
+            spaces: "Number of spaces: ", 
+            spaceNumber: 5,
+            image:"./Images/french.png", 
+
+
+        },
+        {
+            id: 110, 
+            title:"Java",
+            location:"Location: Leicster",
+            price: "Price: £" + 130,
+            spaces: "Number of spaces: ", 
+            spaceNumber: 5,
+            image:"./Images/java.png", 
+
+
+        }
+    ]
+}
